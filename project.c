2337f0672634bc3e92c1bf0b4d34e6e5af052a91
Sahar <applestore@faran-2.local>
2018-01-28 16:13:32 +0330

    source code

--- project.c
+++ project.c
@@ -17,7 +17,7 @@
 enum KEYS{up = 0 , down , left , right};
 int key[4] = {0 , 0 , 0 , 0};
 
-
+//prototype
 int init();
 void wellcome();
 void read_map();
@@ -42,7 +42,7 @@ void init_enemy(ENEMIES *enemy);
 void draw_enemy(ENEMIES *enemy);
 void move_enemy(ENEMIES enemy[],int size);
 int make_random(int min,int max);
-void init_image();
+void init_images();
 //void detection();
 
 
@@ -50,12 +50,20 @@ void init_image();
 
 
 
-
+//Allegro variables
 ALLEGRO_DISPLAY *display = NULL;
 ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 ALLEGRO_TIMER *timer = NULL;
-ALLEGRO_BITMAP *image = NULL;
+ALLEGRO_BITMAP *image_player = NULL;
+ALLEGRO_BITMAP *image_enemy0 = NULL;
+ALLEGRO_BITMAP *image_enemy1 = NULL;
+ALLEGRO_BITMAP *image_enemy2 = NULL;
+ALLEGRO_BITMAP *image_enemy3 = NULL;
+ALLEGRO_BITMAP *image_over = NULL;
+ALLEGRO_BITMAP *image_win = NULL;
+ALLEGRO_BITMAP *image_background = NULL;
 
+//variables
 int cnt_of_move; // player should win in at last cnt_of_move step
 int n,m,map[MAXN][MAXN] = {0};  // n * m is size of map in real
 int N,M; // N * M is size of map in computer
@@ -64,34 +72,57 @@ int s_cell = 100 , l_line = 6; // size of cell in map is s_cell * s_cell , lengt
 int st_x , st_y; // start point of map in allegro display
 int w,h; // w * h is the size of map in allegro display
 int FPS = 60; // frames per second
-int image_width,image_height;
+int image_width_player,image_height_player;
+int image_width_enemy0 , image_height_enemy0;
+int image_width_enemy1 , image_height_enemy1;
+int image_width_enemy2 , image_height_enemy2;
+int image_width_enemy3 , image_height_enemy3;
+int image_width_win , image_height_win;
+int image_width_over , image_height_over;
+int image_width_background , image_height_background;
+
+
 
 int main(void){
+	//object variables
 	PLAYER player; 
 	ENEMIES enemy[E];
+
+
+
 	al_init_font_addon();
 	al_init_ttf_addon();
 	al_init_image_addon();
 
+
 	read_map();
+
+
 	if(!init())
 		return -1;
-	image = al_load_bitmap("P.jpg");
 
-	image_width = al_get_bitmap_width(image);
-	image_height = al_get_bitmap_height(image);
-
-	//wellcome();
-	
+	init_images();
+	wellcome();
 	draw_map();
 	call_enemy(enemy , E);
 	draw_player(&player);
 	init_player(&player);
 	player_move_keyboard(&player,enemy,E);
-	//player_move_mouse();
+	//player_move_mouse();	
+
+
+	//destroy and unistal
+	al_destroy_bitmap(image_player);
+	al_destroy_bitmap(image_enemy0);
+	al_destroy_bitmap(image_enemy1);
+	al_destroy_bitmap(image_enemy2);
+	al_destroy_bitmap(image_enemy3);
+	al_destroy_bitmap(image_over);
+	al_destroy_bitmap(image_background);
+	//al_destroy_bitmap(image_win);
 	al_destroy_display(display);
 	al_rest(12.0);
-	al_destroy_bitmap(image);
+
 	return 0;
 }
 int init(){
@@ -100,8 +131,7 @@ int init(){
 		//al_show_native_message_box(NULL , NULL , NULL , "failed to initialize allegro!" , NULL , NULL);
 		return 0;
 	}
-	display = al_create_display(W / 2 , H / 2
-		);
+	display = al_create_display(W  / 2, H / 2);
 	if(!display){
 		//al_show_native_message_box(NULL , NULL , NULL , "failed to initialize display!" , NULL , NULL);
 		return 0;
@@ -111,10 +141,10 @@ int init(){
 }
 void wellcome(){
 
-	ALLEGRO_FONT *arial = al_load_font("arial.ttf" , 100 , 0);
+	/*ALLEGRO_FONT *arial = al_load_font("arial.ttf" , 100 , 0);
 	ALLEGRO_FONT *papyrus = al_load_font("PAPYRUS.TTF" , 100 , 0);
 
-	/*al_clear_to_color(al_map_rgb(12,12 ,12));
+	al_clear_to_color(al_map_rgb(12,12 ,12));
 	al_draw_text(arial , al_map_rgb(255 , 155 ,149) , W / 2 , H / 2 , 0 , "wellcome");
 	al_flip_display();
 	//al_rest(2.0);
@@ -124,6 +154,10 @@ void wellcome(){
 	al_draw_text(arial , al_map_rgb(0 , 0 ,149) , W / 2 , H / 2 , 0 , "to my game");
 	al_flip_display();
 	al_rest(2.0);*/
+	al_draw_scaled_bitmap(image_background , 0 , 0 , image_width_background , image_height_background , 0  , 0 , W  , H ,  0);
+	al_flip_display();
+	al_rest(5.0);
+
 }
 void read_map(){
 	scanf("%d %d\n", &n , &m);
@@ -167,10 +201,11 @@ void read_map(){
 void draw_map(){	
 	w = (s_cell + l_line) * m;
 	h = (s_cell + l_line) * n;
-	st_x = W / 2 - w / 2 ;
-	st_y = H / 2 - h / 2 ;
+	st_x = (W / 2) - (w / 2);
+	st_y = (H / 2) - (h / 2);
 	al_clear_to_color(al_map_rgb(10 , 10 , 10));
 	al_draw_rectangle(st_x , st_y , st_x + w  , st_y + h  , al_map_rgb(100 , 100 , 100) , l_line);
+	printf("%d %d %d %d %d %d\n",w,h,W,H,st_x,st_y);
 	for(int i = 1 ; i < n  ; i++){
 		int st_line_y = st_y + i * (s_cell + l_line);
 		al_draw_line(st_x , st_line_y , st_x + w , st_line_y  , al_map_rgb( 200 , 200 , 200) , l_line);
@@ -200,10 +235,6 @@ void draw_map(){
 	//al_rest(15.0);
 }
 void back_screen(){
-	w = (s_cell + l_line) * m;
-	h = (s_cell + l_line) * n;
-	st_x = W / 2 - w / 2;
-	st_y = H / 2 - h / 2;
 	al_clear_to_color(al_map_rgb(10 , 10 , 10));
 	al_draw_rectangle(st_x , st_y , st_x + w  , st_y + h  , al_map_rgb(100 , 100 , 100) , l_line);
 	for(int i = 1 ; i < n  ; i++){
@@ -261,22 +292,24 @@ void player_move_keyboard(PLAYER *player,ENEMIES enemy[],int size){
 			move_enemy(enemy,size);
 		}
 		if(enemy->x == player->x &&  enemy->y == player->y){
-			 al_clear_to_color(al_map_rgb(100 , 30 , 30));
+			 	/*al_clear_to_color(al_map_rgb(100 , 30 , 30));
 				ALLEGRO_FONT *arial = al_load_font("arial.ttf" , 60 , 0);
-				al_draw_text(arial , al_map_rgb(255 , 155 ,149) , W / 2 , H / 2 , 0 , "LOSE");
+				al_draw_text(arial , al_map_rgb(255 , 155 ,149) , W / 2 , H / 2 , 0 , "LOSE");*/
+				al_draw_scaled_bitmap(image_over , 0 , 0 , image_width_over , image_height_over , 0  , 0, W , H ,  0);
 				al_flip_display();
 				al_clear_to_color(al_map_rgb(100 , 30 , 30));
-				al_rest(2.0);
+				al_rest(5.0);
 				return;
 		}
 		int x_map,y_map;
 		convert_display_map(player->x,player->y,&x_map,&y_map);
 		if(x_map == 2 * n - 1 && y_map == 2 * m - 1){
-			al_clear_to_color(al_map_rgb(30 , 30 , 100));
+			/*al_clear_to_color(al_map_rgb(30 , 30 , 100));
 			ALLEGRO_FONT *arial = al_load_font("arial.ttf" , 60 , 0);
-			al_draw_text(arial , al_map_rgb(255 , 155 ,149) , W / 2 , H / 2 , 0 , "WIN");
+			al_draw_text(arial , al_map_rgb(255 , 155 ,149) , W / 2 , H / 2 , 0 , "WIN");*/
+			al_draw_scaled_bitmap(image_win , 0 , 0 , image_width_win , image_height_win , 0  , 0, W , H ,  0);
 			al_flip_display();
-			al_clear_to_color(al_map_rgb(30 , 30 , 100));
+			//al_clear_to_color(al_map_rgb(30 , 30 , 100));
 			al_rest(2.0);
 			return;
 		}
@@ -357,7 +390,7 @@ void convert_map_display(int x_map,int y_map,int *x_display , int *y_display){
 }
 void draw_player(PLAYER *player){
 	//al_draw_filled_circle(player->x + 50 , player->y + 50 , 50 , al_map_rgb(56, 45 , 90));
-	al_draw_scaled_bitmap(image, 0 , 0 , image_width , image_height, player->x  , player->y ,(player->x) + (s_cell / 2) , (player->y) + (s_cell / 2) ,  0);
+	al_draw_scaled_bitmap(image_player , 0 , 0 , image_width_player , image_height_player , player->x + l_line  , player->y + l_line , s_cell  , s_cell ,  0);
 }
 int make_random(int min,int max){
     int r;
@@ -402,6 +435,7 @@ void init_enemy(ENEMIES *enemy){
 }
 void draw_enemy(ENEMIES *enemy){
 	al_draw_filled_circle(enemy->x + 50 ,enemy->y + 50 , 40 , al_map_rgb(255 , 10 , 10));
+	al_draw_scaled_bitmap(image_enemy0 , 0 , 0 , image_width_enemy0 , image_height_enemy0 , enemy->x + l_line  , enemy->y + l_line , s_cell  , s_cell ,  0);
 }
 int move_up_enemy(ENEMIES *enemy){
 	int x_map , y_map;
@@ -468,13 +502,54 @@ void move_enemy(ENEMIES enemy[] , int size){
 		}
 	}
 }
+void init_images(){
+	image_player = al_load_bitmap("P.jpg");
+
+	image_width_player = al_get_bitmap_width(image_player);
+	image_height_player = al_get_bitmap_height(image_player);
+
+
+	image_enemy0 = al_load_bitmap("PEO.jpg");
+
+	image_width_enemy0 = al_get_bitmap_width(image_enemy0);
+	image_height_enemy0 = al_get_bitmap_height(image_enemy0);
+
+
+	image_enemy1 = al_load_bitmap("PER.jpg");
+
+	image_width_enemy1 = al_get_bitmap_width(image_enemy1);
+	image_height_enemy1 = al_get_bitmap_height(image_enemy1);
+
+	image_enemy2 = al_load_bitmap("PEB.jpg");
+
+	image_width_enemy2 = al_get_bitmap_width(image_enemy2);
+	image_height_enemy2 = al_get_bitmap_height(image_enemy2);
+
+	image_enemy3 = al_load_bitmap("PEP.jpg");
+	image_width_enemy3 = al_get_bitmap_width(image_enemy3);
+	image_height_enemy3 = al_get_bitmap_height(image_enemy3);
+
+	image_over = al_load_bitmap("gameover.jpg");
+	image_width_over = al_get_bitmap_width(image_over);
+	image_height_over = al_get_bitmap_height(image_over);
+
+	image_win = al_load_bitmap("win.jpg");
+	image_width_win = al_get_bitmap_width(image_win);
+	image_height_win = al_get_bitmap_height(image_win);
+
+	image_background = al_load_bitmap("background.jpg");
+	image_width_background = al_get_bitmap_width(image_background);
+	image_height_background = al_get_bitmap_height(image_background);
+}
 
 /*
 sample tests 
 test #1;
-1 3 10 
+2 3 10 
 ._._._.
 | | . |
+._._._.
+| . . |
 ._._._.
 
  
@@ -490,7 +565,23 @@ test #2;
 | . . . |
 ._._._._.
     
+
+
+    
+    
+
 */
+
+
+
+
+
+
+
+
+
+
+
 
 
 
